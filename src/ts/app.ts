3c55f9fd20a9864889e02961182393a15d83865b
Byron Wall <byron@byroni.us>
2017-03-05 21:23:19 -0700

    Add support for clearing pieces and shifting

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -14,31 +14,141 @@ class Board {
     //hold some pieces
     width: number;
     height: number;
+    colors: number;
 
     pieces: Array<Array<Piece>>;
 
-    constructor(width: number, height: number) {
+    constructor(width: number, height: number, colors = 3) {
         this.width = width;
         this.height = height;
+        this.colors = colors;
 
         this.pieces = [];
 
         //create the pieces
         for (var x of _.range(width)) {
 
-            let row = [];
-            this.pieces[x] = row;
-            
+            let column = [];
+            this.pieces[x] = column;
+
             for (var y of _.range(height)) {
                 //create a piece, add to Array
-                let piece = new Piece(x, y);
+                var color = _.random(this.colors)
+                let piece = new Piece(x, y, color);
+                column.push(piece);
+            }
+        }
+    }
 
-                row.push(piece);
+    private _removePiece(piece: Piece) {
+        this.pieces[piece.x][piece.y] = null;
+    }
+
+    private _shiftDownAndLeft() {
+        //this will take the board and shift pieces down through blanks
+        let shiftLeft = 0;
+        for (let x = 0; x < this.width; x++) {
+            let shiftDown = 0;
+            let wereAllBlank = true;
+            for (let y = this.height - 1; y >= 0; y--) {
+                let piece = this.pieces[x][y];
+                if (piece == null) {
+                    shiftDown++;
+                    continue;
+                }
+                wereAllBlank = false;
+                if (shiftDown > 0 || shiftLeft > 0) {
+                    piece.y = y + shiftDown;
+                    piece.x = x - shiftLeft
+                    this.pieces[x - shiftLeft][y + shiftDown] = piece;
+                    this.pieces[x][y] = null;
+                }
+            }
+
+            if(wereAllBlank){
+                shiftLeft++;
+            }
+        }
+    }
+
+    private _getPieces(): Array<Piece> {
+        let nonNullPieces: Array<Piece> = [];
+        for (var col of this.pieces) {
+            for (var piece of col) {
+                if (piece != null) {
+                    nonNullPieces.push(piece);
+                }
+            }
+        }
+        return nonNullPieces;
+    }
+
+    removePiece(piece: Piece) {
+        //search the neighbors of this piece for same color
+
+        let neighbors = this._getNeighbors(piece);
+
+        let neighborsTested: Array<Piece> = [piece];
+
+        let didRemovalHappen = false;
+        let wasRemoved = false;
+
+        while (neighbors.length) {
+            let neighborTest = neighbors.pop();
+
+            neighborsTested.push(neighborTest);
+
+            if (neighborTest.color == piece.color) {
+                didRemovalHappen = true;
+                this._removePiece(neighborTest)
+
+                for (var nextNeighbor of this._getNeighbors(neighborTest)) {
+                    if (neighborsTested.indexOf(nextNeighbor) == -1) {
+                        neighbors.push(nextNeighbor);
+                    }
+                }
+            }
+
+            if (didRemovalHappen && !wasRemoved) {
+                this._removePiece(piece);
+                wasRemoved = true;
             }
         }
 
-        console.log(this.pieces);
+        this._shiftDownAndLeft();
+    }
 
+    private _getNeighbors(piece: Piece): Array<Piece> {
+        //return the neighbors of this Piece
+
+        let deltas = [
+            { dx: 1, dy: 0 },
+            { dx: -1, dy: 0 },
+            { dx: 0, dy: 1 },
+            { dx: 0, dy: -1 }
+        ];
+
+        let neighbors: Array<Piece> = [];
+
+        _.each(deltas, (delta) => {
+            let xTest = piece.x + delta.dx;
+            let yTest = piece.y + delta.dy;
+
+            if (this._isValidPiece(xTest, yTest)) {
+                neighbors.push(this.pieces[xTest][yTest]);
+            }
+        })
+
+        return neighbors;
+
+    }
+
+    private _isValidPiece(x: number, y: number): boolean {
+        return this._withinBounds(x, y) && this.pieces[x][y] != null;
+    }
+
+    private _withinBounds(x: number, y: number): boolean {
+        return (x < this.width && y < this.height && x >= 0 && y >= 0);
     }
 
     private _renderDetails() {
@@ -47,30 +157,26 @@ class Board {
         var pieceWidth = +svg.attr("width") / this.width;
         var pieceHeight = +svg.attr("height") / this.height;
 
-        var d3Cols = svg.selectAll('g')
-            .data(this.pieces)
+        var d3Cell = svg.selectAll('rect')
+            .data(this._getPieces())
 
-        d3Cols.enter()
-            .append('g')
-
-        var d3Cell = d3Cols
-            .selectAll('rect')
-            .data((d) => { return d; })
-
-        d3Cell
-            .enter()
+        d3Cell.enter()
             .append('rect')
+
+        d3Cell.exit().remove();
+
+        var color = d3.scaleOrdinal(d3.schemeCategory10);
 
         //update
         d3Cell.attr('x', (d) => { return d.x * pieceWidth; })
             .attr('y', (d) => { return d.y * pieceHeight; })
             .attr('width', () => { return pieceWidth; })
             .attr('height', () => { return pieceHeight; })
-            .attr('fill', (d) => { return (d.isSelected) ? "#f00" : "#aaa"; })
-            .attr('stroke', '#fff')
+            .attr('stroke', (d) => { return (d.isSelected) ? "#000" : "#fff"; })
+            .attr('fill', (d) => { return color(String(d.color)) })
             .on("click", (d) => {
                 console.log(d);
-                d.isSelected = true;
+                this.removePiece(d);
                 this._renderDetails();
             });
     }
@@ -96,10 +202,12 @@ class Piece {
     x: number;
     y: number;
     isSelected: boolean;
+    color: number;
 
-    constructor(x: number, y: number) {
+    constructor(x: number, y: number, color: number) {
         this.x = x;
         this.y = y;
+        this.color = color;
     }
     //some functions to trigger on events
 }
